af3fdf2ad3e6fd1c01ab032bcd7ebbd4651c5623
LokankshiSharma <81632228+LokankshiSharma@users.noreply.github.com>
2023-08-07 10:36:25 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,47 @@
-function setup(){
-    canvas=createCanvas(500, 500);
-  canvas.center;
-  video=createCapture( VIDEO );
-  video.size(550, 500);
-  PoseNet=ml5.poseNet(video, modelLoaded);
-  PoseNet.on("pose", gotResult)
-function draw(){
-    image(video, 0, 0, 350, 350 );
+var song1 = "";
+var song2 = "";
+var leftWristX = 0;
+var leftWristY = 0;
+var rightWristX = 0;
+var rightWristY = 0;
+var leftWscore = 0;
+var rightWscore = 0;
 
-function modelLoaded(){
-    console.log('PoseNet is Initialized');
+function setup(){
+        canvas = createCanvas(600, 500);
+	  canvas.center();
+	  video = createCapture(VIDEO);
+	  video.hide();
+	  poseNet = ml5.poseNet(video, modelLoaded);
+	  poseNet.on('pose', gotPoses);
+    
 }
 function preload(){
-    song = loadSound("AIIYL.mp3");
-    song = loadSound("Antifragile.mp3");
+      song1 = loadSound("AIIYL.mp3");
+      song2 = loadSound("Antifragile.mp3");
 }
+
+function play(){
+      song1.play();
+      song1.setVolume(1);
+
+}
+
+function draw(){
+      image (video, 0, 0, 600, 500);
+      fill('red');
+      stroke('white');
+      if (leftWscore > 0.2){
+      circle (leftWristX, leftWristY, 20)
+      song1.play();   
+      }
+}
+    
+
+function modelLoaded(){
+    console.log('PoseNet is initialized')
+}
+
 function gotPoses(results){
     if (results.length>0){
         console.log(results)
@@ -24,8 +51,9 @@ function gotPoses(results){
         rightWristY=results[0].pose.rightWrist.y;
         console.log('Left Wrist X & Y = ' + leftWristX + ',' + leftWristY);
         console.log(' Right Wrist X & Y = ' + rightWristX + ',' + rightWristY);
+        leftWscore = results[0].pose.keypoints[9].score;
+        rightWscore = results[0].pose.keypoints[10].score;
+        console.log('Right Wrist Score: ' + rightWscore + ' Left Wrist Score: ' + leftWscore);
     }
 }
 
-var song1="AIIYL.mp3"
-var song2="Antifragile.mp3"
